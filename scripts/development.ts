e53d69772889912b6d0c01a1c4d5775a7009bcf2
fightant1w1ll <nc_8093@163.com>
2022-10-08 22:00:36 +0800

    Add watch function

--- scripts/development.ts
+++ scripts/development.ts
@@ -1,5 +1,24 @@
 import express from 'express';
 import path from 'path';
+import fs from 'fs/promises';
+import { build } from './build';
+
+// watch the changes and restart the dev server
+async function watchSourceChange() {
+  const cwd = process.cwd();
+  const events = fs.watch(path.resolve(cwd, 'packages'), {
+    recursive: true,
+  });
+
+  for await (const e of events) {
+    console.log(`[FILE CHANGE] detected: ${e.eventType}-${e.filename}`);
+    if (e.eventType === 'change') {
+      build().then(() => {
+        console.log('[UPDATE] site is SUCCESSFUL');
+      });
+    }
+  }
+}
 
 const app = express();
 const port = 3301;
@@ -8,4 +27,5 @@ app.use(express.static(path.resolve(process.cwd(), 'dist')));
 
 app.listen(port, () => {
   console.log(`[DEV-SERVER] is listening on port ${port}`);
+  // watchSourceChange().then(() => console.log('[WATCHING] site changes'));
 });
